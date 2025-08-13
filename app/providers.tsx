4675b3f580e7da34e3423699b5e01703fdbe82d0
yunus-0x <laguvist@gmail.com>
2025-08-13 14:22:29 +0700

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,9 +1,16 @@
 'use client';
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useState } from "react";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { useState } from 'react';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  const [qc] = useState(() => new QueryClient());
-  return <QueryClientProvider client={qc}>{children}</QueryClientProvider>;
+  const [client] = useState(() => new QueryClient());
+  return (
+    <QueryClientProvider client={client}>
+      {children}
+      {/* enable when debugging */}
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+    </QueryClientProvider>
+  );
 }
